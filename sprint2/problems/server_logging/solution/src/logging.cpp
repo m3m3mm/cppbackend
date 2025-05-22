5b449b990d9fd5c5f69472a157d16ce80d85f5c2
m3m3mm <wolrusteen@ya.ru>
2025-05-22 14:04:18 +0000

    7/8

--- sprint2/problems/server_logging/solution/src/logging.cpp
+++ sprint2/problems/server_logging/solution/src/logging.cpp
@@ -5,38 +5,59 @@
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/utility/setup/file.hpp>
 #include <boost/json.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/utility/setup/console.hpp>
 
 namespace logging {
 
 void InitLogging() {
     boost::log::add_common_attributes();
+    
+    // Remove any existing sinks
+    boost::log::core::get()->remove_all_sinks();
+    
+    // Add a console sink that only outputs the message
+    auto console_sink = boost::log::add_console_log();
+    console_sink->set_formatter(
+        boost::log::expressions::stream << boost::log::expressions::smessage
+    );
 }
 
 void LogRequest(std::string_view remote_addr, std::string_view uri, std::string_view method) {
-    boost::json::object request;
-    request["remote_addr"] = std::string(remote_addr);
-    request["uri"] = std::string(uri);
-    request["method"] = std::string(method);
+    boost::json::object data;
+    data["method"] = std::string(method);
+    data["URI"] = std::string(uri);
     
-    BOOST_LOG_TRIVIAL(info) << "request: " << boost::json::serialize(request);
+    boost::json::object log;
+    log["message"] = "request received";
+    log["data"] = data;
+    
+    BOOST_LOG_TRIVIAL(info) << boost::json::serialize(log);
 }
 
 void LogResponse(std::string_view remote_addr, int response_time, int response_code, std::string_view content_type) {
-    boost::json::object response;
-    response["remote_addr"] = std::string(remote_addr);
-    response["response_time"] = response_time;
-    response["response_code"] = response_code;
-    response["content_type"] = std::string(content_type);
+    boost::json::object data;
+    data["code"] = response_code;
+    data["content_type"] = std::string(content_type);
     
-    BOOST_LOG_TRIVIAL(info) << "response: " << boost::json::serialize(response);
+    boost::json::object log;
+    log["message"] = "response sent";
+    log["data"] = data;
+    
+    BOOST_LOG_TRIVIAL(info) << boost::json::serialize(log);
 }
 
 void LogServerStart(unsigned short port, std::string_view address) {
-    boost::json::object server;
-    server["port"] = port;
-    server["address"] = std::string(address);
+    boost::json::object data;
+    data["port"] = port;
+    data["address"] = std::string(address);
     
-    BOOST_LOG_TRIVIAL(info) << "server_start: " << boost::json::serialize(server);
+    boost::json::object log;
+    log["message"] = "server started";
+    log["data"] = data;
+    
+    BOOST_LOG_TRIVIAL(info) << boost::json::serialize(log);
 }
 
 void LogServerStop(int code, const std::exception* ex) {
@@ -46,16 +67,24 @@ void LogServerStop(int code, const std::exception* ex) {
         data["exception"] = ex->what();
     }
     
-    BOOST_LOG_TRIVIAL(info) << "server_stop: " << boost::json::serialize(data);
+    boost::json::object log;
+    log["message"] = "server stopped";
+    log["data"] = data;
+    
+    BOOST_LOG_TRIVIAL(info) << boost::json::serialize(log);
 }
 
 void LogError(int code, std::string_view text, std::string_view where) {
-    boost::json::object error;
-    error["code"] = code;
-    error["text"] = std::string(text);
-    error["where"] = std::string(where);
+    boost::json::object data;
+    data["code"] = code;
+    data["text"] = std::string(text);
+    data["where"] = std::string(where);
     
-    BOOST_LOG_TRIVIAL(error) << "error: " << boost::json::serialize(error);
+    boost::json::object log;
+    log["message"] = "error occurred";
+    log["data"] = data;
+    
+    BOOST_LOG_TRIVIAL(error) << boost::json::serialize(log);
 }
 
 } // namespace logging 
