ca345d3db6c0f62258f5053f7dd25bffdd6ef55e
Kotelenets Emm <wolrusteen@yandex.ru>
2025-06-11 11:41:54 +0200

    fix 2 + 3rd solution

--- sprint2/problems/static_content/solution/src/http_server.cpp
+++ sprint2/problems/static_content/solution/src/http_server.cpp
@@ -1,67 +1,66 @@
+
+#include <boost/asio/dispatch.hpp>
+
+#include <iostream>
+
 #include "http_server.h"
-#include "logger.h"
-#include "json_helper.h"
 
 namespace http_server {
 
 void ReportError(beast::error_code ec, std::string_view what) {
-    logger::LogJsonAndMessage(json_helper::CreateNetworkErrorValue(ec.value(), ec.message(), std::string(what)), "error");
-}
-
-void ReportRequest(const tcp::endpoint& endpoint, std::string_view uri, std::string_view method) {
-    logger::LogJsonAndMessage(json_helper::CreateRequestValue(endpoint, std::string(uri), std::string(method)), "request received");
+    std::cerr << what << ": "sv << ec.message() << std::endl;
 }
 
 void SessionBase::Run() {
-    // Запуск метода Read с использованием executor объекта stream_.
-    // Вся работа со stream_ будет выполняться через его executor
+    // Вызываем метод Read, используя executor объекта stream_.
+    // Таким образом вся работа со stream_ будет выполняться, используя его executor
     net::dispatch(stream_.get_executor(),
-                    beast::bind_front_handler(&SessionBase::Read, GetSharedThis()));
+                  beast::bind_front_handler(&SessionBase::Read, GetSharedThis()));
 }
 
 void SessionBase::Read() {
-    // Сброс запроса (метод Read может быть вызван несколько раз)
+    using namespace std::literals;
+    // Очищаем запрос от прежнего значения (метод Read может быть вызван несколько раз)
     request_ = {};
     stream_.expires_after(30s);
-    // Чтение request_ из stream_ с использованием buffer_ для хранения данных
+    // Считываем request_ из stream_, используя buffer_ для хранения считанных данных
     http::async_read(stream_, buffer_, request_,
-                        // По завершении операции вызывается метод OnRead
-                        beast::bind_front_handler(&SessionBase::OnRead, GetSharedThis()));
+                     // По окончании операции будет вызван метод OnRead
+                     beast::bind_front_handler(&SessionBase::OnRead, GetSharedThis()));
 }
 
-void SessionBase::OnRead(beast::error_code ec, [[maybe_unused]] std::size_t bytes_read) {
+void SessionBase::OnRead(beast::error_code ec, std::size_t bytes_read) {
+    using namespace std::literals;
     if (ec == http::error::end_of_stream) {
-        // Обычная ситуация — клиент закрыл соединение
+        // Нормальная ситуация - клиент закрыл соединение
         return Close();
     }
-    
     if (ec) {
         return ReportError(ec, "read"sv);
-    } else {
-        ReportRequest(stream_.socket().local_endpoint(), request_.target(), request_.method_string());
     }
-    
-
     HandleRequest(std::move(request_));
-}
-
-void SessionBase::OnWrite(bool close, beast::error_code ec, [[maybe_unused]] std::size_t bytes_written) {
-    if (ec) {
-        return ReportError(ec, "write"sv);
-    }
-
-    if (close) {
-        // По семантике ответа требуется закрыть соединение
-        return Close();
-    }
-
-    // Чтение следующего запроса
-    Read();
 }
 
 void SessionBase::Close() {
     beast::error_code ec;
     stream_.socket().shutdown(tcp::socket::shutdown_send, ec);
+
+    if(ec) {
+        ReportError(ec, "close"s);
+    }
 }
 
-}  // namespace http_server
+void SessionBase::OnWrite(bool close, beast::error_code ec, std::size_t bytes_written) {
+    if (ec) {
+        return ReportError(ec, "write"sv);
+    }
+
+    if (close) {
+        // Семантика ответа требует закрыть соединение
+        return Close();
+    }
+
+    // Считываем следующий запрос
+    Read();
+}
+} // namespace http_server
--- sprint2/problems/static_content/solution/src/json_loader.h
+++ sprint2/problems/static_content/solution/src/json_loader.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #include <filesystem>
-#include "game.h"
+#include <fstream>
+
+#include "model.h"
 
 namespace json_loader {
-    
 model::Game LoadGame(const std::filesystem::path& json_path);
-
 }  // namespace json_loader
